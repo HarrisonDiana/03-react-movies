3158f29cf78442f5c2ea236b812b01f16f7c2931
Harrison Diana <dharrison77777@gmail.com>
2026-02-09 20:03:53 +0100

    add index file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import toast from 'react-hot-toast';
 
-import SearchBar from './components/SearchBar/SearchBar';
-import MovieGrid from './components/MovieGrid/MovieGrid';
-import MovieModal from './components/MovieModal/MovieModal';
-import Loader from './components/Loader/Loader';
-import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import MovieModal from '../MovieModal/MovieModal';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
-import { fetchMovies } from './services/movieService';
-import type { Movie } from './types/movie';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+
 
 import styles from './App.module.css';
 
